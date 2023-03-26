e3cbf620bc0789dd93189f09b13483f277d11ed7
Sanchit Tewari <46245135+sanchitttt@users.noreply.github.com>
2023-03-26 06:52:21 +0530

    Update index.js

--- timetableApp/main/frontend/src/pages/room_preferences_page/mobile/index.js
+++ timetableApp/main/frontend/src/pages/room_preferences_page/mobile/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Text24px from '../../../common/text/Text24px';
 
 function RoomPreferencesMobile() {
   return (
-    <div>index</div>
+    <div style={{ textAlign: 'center' }} className='w-[100%] h-[100%] flex items-center justify-center '>
+      <Text24px>Please use a desktop screen or tablet for viewing this page</Text24px>
+    </div>
   )
 }
 
